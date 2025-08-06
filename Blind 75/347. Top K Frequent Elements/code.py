b3bb5c06d6fc9fea1c71186d2ff542be3605e971
rapha <onanaraphaelnathan@gmail.com>
2025-08-06 18:37:56 -0400

    Correct solution for leetcode 347

--- Blind 75/347. Top K Frequent Elements/code.py
+++ Blind 75/347. Top K Frequent Elements/code.py
@@ -1,17 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
+        if n == 1: return nums
+
         num_freq = {}
-        buckets = [[]] * n
+        buckets = [[] for _ in range(n + 1)]
         output = []
 
         for num in nums:
             num_freq[num] = 1 + num_freq.get(num, 0)
 
-        for key in nums:
-            buckets[num_freq[key]] = key
+        for num, freq in num_freq.items():
+            buckets[freq].append(num)
 
-        for i in range(n-1, 0, -1):
+        for i in range(n, 0, -1):
             for val in buckets[i]:
                 output.append(val)
                 if len(output) == k: return output
